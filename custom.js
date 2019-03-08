d1d4703fb8b82c60bde934daaee2788f9ec386ca
Coleman <33269438+E-Cole-I@users.noreply.github.com>
2019-03-08 13:37:37 -0600

    Added more comments

--- custom.js
+++ custom.js
@@ -6,9 +6,14 @@ require(
             $(jupyter.events).on("kernel_ready.Kernel", function () {
 
                 // Run all the cells so the output is there
+                // This was useful in my project, but it may not be in yours.
+                // To avoid this, a simple fix would be to delete this and run the cells before tagging them.
                 console.log("Auto-running all cells-below...");
                 jupyter.actions.call('jupyter-notebook:run-all-cells-below');
                 jupyter.actions.call('jupyter-notebook:save-notebook');
+                //-----------------------------------------------------------------------------------------
+
+                // Hide the cells based on Tags set in Jupyter Notebook
 
                 //  Retrieve all of the elements with a class name cell-tag (View -> Cell Toolbar -> Tags)
                 var x = document.getElementsByClassName("cell-tag");
@@ -28,13 +33,18 @@ require(
                     var p5 = '.inner_cell';
                     var p6 = '.input';
 
-                    // I used the tag "hidecode", but others can be used 
+                    // I used the tag "hidecode", but others can be used.
                     if (xSplit == "hidecode") {
                           // Use JQuery to walk up the directory using the variables from above and change the display css to none. 
                           $(x[i]).parent(p1).parent(p2).parent(p3).parent(p4).parent(p5).parent(p6).css( "display", "none" );
                         
                       }
+                    // Other tags can be added with more elif and else statements. Just copy what's above. 
                     }
             });
+
+            // Contact me (Coleman) @ colemanshepard@gmail.com with any questions. 
+            // Please add a descriptive tag line if you do!
+            // Happy Coding!
         }
     );
